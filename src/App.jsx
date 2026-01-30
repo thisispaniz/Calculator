dcf046070f7a538c063e22595ef29d5a08814f45
thisispaniz <paniz.ziaie@gmail.com>
2026-01-30 16:11:20 +0100

    chore: something changed

--- src/App.jsx
+++ src/App.jsx
@@ -62,7 +62,7 @@ function App() {
 
   return (
     <div className="calculator-grid">
-      <h2>Hello!</h2>
+      <h2>Medical Calculator!</h2>
       <div className="output">
         <div className="previous-operand">{previous} {operation}</div>
         <div className="current-operand" data-testid="display">{current}</div>
